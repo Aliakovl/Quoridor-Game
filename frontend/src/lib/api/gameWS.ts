48193c79d86c3c9759fc4cefe3de4255c41dcc23
Aleksei Iakovlev <mister.yakowlev2011@gmail.com>
2023-04-01 01:20:48 +0300

    game-history-front

--- frontend/src/lib/api/gameWS.ts
+++ frontend/src/lib/api/gameWS.ts
@@ -1,10 +1,12 @@
 import type {Game, Move} from "./types";
+import {dev} from "$app/environment";
 
 export class GameWS {
     private ws: WebSocket;
 
     constructor(gameId: string, token: string, getGame: (game: Game) => void) {
-        this.ws = new WebSocket(`ws://localhost/ws/game/${gameId}?token=${token}`);
+        const ws = dev ? "ws" : "wss";
+        this.ws = new WebSocket(`${ws}://${location.host}/ws/game/${gameId}?token=${token}`);
         this.ws.onmessage = message => {
             getGame(JSON.parse(message.data.toString()) as Game);
         }
